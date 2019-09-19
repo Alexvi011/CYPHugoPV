3f881f882ac94601e4245ef3acfead77fbc2214c
Alexvi011 <alejandropvite@gmail.com>
2019-09-19 08:11:17 -0500

    Se agregan ejecicios funcionando

---
+++ libro/ejemplo1_13.py
@@ -0,0 +1,8 @@
+MAT= int(input("Ingresa tu matricula de alumno:"))
+CAL1 = int(input("Ingresa tu calificacion 1:"))
+CAL2 = int(input("Ingresa tu calificacion 2:"))
+CAL3 = int(input("Ingresa tu calificacion 3:"))
+CAL4 = int(input("Ingresa tu calificacion 4:"))
+CAL5 = int(input("Ingresa tu calificacion 5:"))
+PRO= (CAL1+CAL2+CAL3+CAL4+CAL5)/5
+print(f"Tu matricula es:{MAT}, y tu promedio es:{PRO}")          
