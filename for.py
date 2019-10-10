6ea345e1d6995f3280102be34d2bcbf07654f443
Alexvi011 <alejandropvite@gmail.com>
2019-10-10 17:47:26 -0500

    Se agrega apunte

---
+++ for.py
@@ -0,0 +1,10 @@
+V=0
+F=0
+Z=0
+for V in range(1,11,1):
+    print(V)
+for F in range(1,11,1):
+    print(f"Hola {F}")
+for Z in range(1,11,1):
+    print(f"5 x {Z}={5*Z}")
+
---
+++ for2.py
@@ -0,0 +1,5 @@
+H=int(input("Introduce el numero de la tabla: "))
+Z=0
+for Z in range(1,11,1):
+    print(f"{H} x {Z}={H*Z}")
+
